f6ecce9442dc56628f8c085ee6b41ff5c58bf66c
ImNotMee <philip.zhang@mail.utoronto.ca>
2023-10-10 14:08:20 -0400

    Updated website

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -7,39 +7,31 @@ class Main extends Component {
       <div id="main-body">
         <div class="main">
           <h3 id="main-h3">About me:</h3>
-          <p id="main-p">I am a recent Computer Science graduate from University of Toronto. Currently a Software Engineer.</p>
+          <p id="main-p">University of Toronto Computer Science Alumni, and with 2 years of professional backend focused experiences with Java Spring Boot and 1 year of DevOps experiences. Also experienced in Python and JavaScript with various libraries and frameworks.</p>
           <h3 id="main-h3">Work Experiences:</h3>
-          <p id="main-p">Software Engineer at UKG <span style={{color: "#388cd6"}}>[Aug 2021 - Present]</span> </p>
-          <p id="main-p">Software Developer Intern at UKG <span style={{color: "#388cd6"}}>[May 2021 - Aug 2021]</span> </p>
-          <p id="main-p">DevOps Engineer Intern at Oracle <span style={{color: "#388cd6"}}>[May 2019 - May 2020]</span> </p>
-          <p id="main-p">Technical Analyst at Toyota Canada Inc <span style={{color: "#388cd6"}}>[Jan 2018 - Apri 2018]</span> </p>
-          <h3 id="main-h3">Extracurricular Activites:</h3>
-          <ul id="lst">
-            <li>VP of Internal Tech and Relations at DSC@UTSC <span style={{color: "#388cd6"}}>[2019-2020]</span></li>
-            <li>Adminstration Assistant at ROCSAUT UTSC <span style={{color: "#388cd6"}}>[2019-2020]</span></li>
-            <li>Assistant Director at UTHKSC <span style={{color: "#388cd6"}}>[2018-2020]</span></li>
-            <li>Software Developer at DSC@UTSC <span style={{color: "#388cd6"}}>[2019]</span></li>
-            <li>Treasurer of Model United Nation Club <span style={{color: "#388cd6"}}>[2015-2016]</span></li>
-            <li>Customer Service Assistant at North York Harvest<span style={{color: "#388cd6"}}>[2015-2016]</span></li>
-            <li>Members of various school bands <span style={{color: "#388cd6"}}>[2012-2016]</span></li>
-            <li>Sergeant at Royal Canadian Air Cadets <span style={{color: "#388cd6"}}>[2010-2014]</span></li>
-          </ul>
+            <p id="main-p">Software Engineer II @ UKG <span style={{color: "#388cd6"}}>[Jan - Aug 2023]</span> </p>
+            <p id="main-p">Software Engineer @ UKG <span style={{color: "#388cd6"}}>[Aug 2021 - Dec 2022]</span> </p>
+            <p id="main-p">Software Developer Intern @ UKG <span style={{color: "#388cd6"}}>[May 2021 - Aug 2021]</span> </p>
+            <p id="main-p">DevOps Engineer Intern @ Oracle <span style={{color: "#388cd6"}}>[May 2019 - May 2020]</span> </p>
+            <p id="main-p">Technical Analyst @ Toyota Canada Inc <span style={{color: "#388cd6"}}>[Jan 2018 - Apri 2018]</span> </p>
           <div id="t">
             <div>
-              <h3 id="main-h3">Languages:</h3>
+              <h3 id="main-h3">Programming Languages:</h3>
               <ul id="lst">
                 <li>Java</li>
                 <li>Python</li>
                 <li>HTML & CSS</li>
                 <li>JavaScript</li>
                 <li>SQL</li>
-                <li>Bash</li>
+                <li>TypeScript</li>
                 <li>C</li>
+                <li>Bash</li>
               </ul>
             </div>
             <div id="box">
               <h3 id="main-h3">Frameworks &amp; Libraries:</h3>
               <ul id="lst">
+                <li>Spring Boot</li>
                 <li>Node.js</li>
                 <li>React js</li>
                 <li>AngularJS</li>
@@ -47,14 +39,20 @@ class Main extends Component {
               </ul>
             </div>
             <div id="box">
-              <h3 id="main-h3">Other Stacks:</h3>
+              <h3 id="main-h3">Tools &amp; Other Tech:</h3>
               <ul id="lst">
-                <li>Git & SVN</li>
+                <li>Git</li>
                 <li>MySQL & SQLite</li>
+                <li>REST API</li>
                 <li>TeamCity</li>
+                <li>Bitbucket</li>
                 <li>Chef</li>
+                <li>Sensu</li>
                 <li>Docker</li>
                 <li>Kubernetes</li>
+                <li>Kafka</li>
+                <li>RabbitMQ</li>
+                <li>MongoDB</li>
               </ul>
             </div>
           </div>
--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -38,9 +38,9 @@ class Portfolio extends Component {
   render() {
     return (
       <div className="Portfolio">
-      <div className="left">
+        <div className="left">
           <div id="logo">
-              <img id="logo" src="icons/logo.jpg" alt="" />
+              <img id="logo" src="icons/picofme.jpg" alt="" />
           </div>
           <div id="contacts" align= "center">
               <a href="https://www.instagram.com/_philzpng_/"><img id="icons" src="icons/instagram.png" alt="" /></a>
@@ -49,7 +49,7 @@ class Portfolio extends Component {
           </div>
           <div id="copyright">
               <p align="center" id="bio">An UofT Alumni</p>
-              <p align="center"><span id="colour">Philip</span> @ 2022</p>
+              <p align="center"><span id="colour">Philip</span> @ 2023</p>
           </div>
           <div className="navbuttons">
             <div className="pad">
@@ -59,10 +59,7 @@ class Portfolio extends Component {
               <button className="button-style" value="projects" onClick={this.updatePage}>Projects</button>
             </div>
             <div className="pad">
-              <button className="button-style" value="photo" onClick={this.updatePage}>Photo</button>
-            </div>
-            <div className="pad">
-              <button className="button-style" value="contact" onClick={() => window.location ='mailto:philip.zhang@mailutoronto.ca'}>Contact me</button>
+              <button className="button-style" value="contact" onClick={() => window.location ='mailto:philip.zhang@mail.utoronto.ca'}>Contact me</button>
             </div>
           </div>
       </div>
